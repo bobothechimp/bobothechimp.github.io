247a5b06d2fdf50a69c1093c8350d67e0cfc70b2
bobothechimp <barnesnoah@icloud.com>
2023-08-01 23:41:26 -0400

    Added test and example components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Header from "./components/Header";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [message, setMessage] = useState("Welcome!");
+  const [showAlert, setShowAlert] = useState(false);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Header />
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>
+          Quick! Dismiss this alert!
+        </Alert>
+      )}
+      <h1>{message}</h1>
+      <Button
+        onClick={() => {
+          setShowAlert(true);
+          setMessage("You win!");
+        }}
+      >
+        Click to <strong>win</strong>
+      </Button>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>BU Smash Society</h1>;
+}
+
+export default Header;
