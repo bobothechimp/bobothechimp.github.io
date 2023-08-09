ccbf775f0db9ecb4059a28067264c85d387bf535
bobothechimp <barnesnoah@icloud.com>
2023-08-09 00:32:34 -0400

    Started working with SQL to store tournament info

--- src/components/TournamentList.tsx
+++ src/components/TournamentList.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { ListGroup, Form } from "react-bootstrap";
+import axios from "axios";
 
 import getTournamentWinners from "../querying/TournamentQueries";
 
@@ -10,10 +11,24 @@ const TournamentList = () => {
   return (
     <>
       <h1>Tournaments</h1>
+      <Form.Select size="sm">
+        <option value="none">Season</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </Form.Select>
+      <Form.Select size="sm">
+        <option value="none">Week</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </Form.Select>
       {winners.map((top3) => (
-        <ListGroup>
+        <ListGroup key={top3.toString()}>
           {top3.map((player) => (
-            <ListGroup.Item>{player}</ListGroup.Item>
+            <ListGroup.Item key={player}>{player}</ListGroup.Item>
           ))}
         </ListGroup>
       ))}
